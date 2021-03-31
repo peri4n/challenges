904910a38f253338aae3c7d7f07ece91f267bf13
Fabian Bull <fabian.bull@bioinform.at>
2021-03-31 18:56:03 +0200

    Add Project Euler problem 17

--- euler/haskell/app/Main.hs
+++ euler/haskell/app/Main.hs
@@ -17,6 +17,7 @@ import Problems.Problem13 as P13
 import Problems.Problem14 as P14
 import Problems.Problem15 as P15
 import Problems.Problem16 as P16
+import Problems.Problem17 as P17
 import Problems.Problem18 as P18
 import Problems.Problem19 as P19
 
@@ -39,6 +40,7 @@ main = do
               "14" -> P14.solution
               "15" -> P15.solution
               "16" -> P16.solution
+              "17" -> P17.solution
               "18" -> P18.solution
               "19" -> P19.solution
               _ -> 0
