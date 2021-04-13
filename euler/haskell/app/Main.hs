1ce05202882d971a7c5123d25f668c20720f8537
Fabian Bull <fabian.bull@bioinform.at>
2021-04-13 19:37:28 +0200

    Add Project Euler problem 36

--- euler/haskell/app/Main.hs
+++ euler/haskell/app/Main.hs
@@ -26,6 +26,7 @@ import Problems.Problem27 as P27
 import Problems.Problem31 as P31
 import Problems.Problem32 as P32
 import Problems.Problem35 as P35
+import Problems.Problem36 as P36
 import Problems.Problem50 as P50
 import Problems.Problem87 as P87
 
@@ -57,6 +58,7 @@ main = do
               "31" -> P31.solve
               "32" -> P32.solve
               "35" -> P35.solve
+              "36" -> P36.solve
               "50" -> P50.solve
               "87" -> P87.solve
               _ -> return 0
--- euler/haskell/test/Spec.hs
+++ euler/haskell/test/Spec.hs
@@ -22,6 +22,7 @@ import Problems.Problem23 as P23
 import Problems.Problem27 as P27
 import Problems.Problem32 as P32
 import Problems.Problem35 as P35
+import Problems.Problem36 as P36
 import Problems.Problem50 as P50
 import Problems.Problem87 as P87
 
@@ -71,6 +72,8 @@ problem32 = TestCase (assertEqual "Solution to problem 32" P32.solution 45228)
 
 problem35 = TestCase (assertEqual "Solution to problem 35" P35.solution 55)
 
+problem36 = TestCase (assertEqual "Solution to problem 35" P36.solution 872187)
+
 problem50 = TestCase (assertEqual "Solution to problem 50" P50.solution 997651)
 
 problem87 = TestCase (assertEqual "Solution to problem 87" P87.solution 1097343)
@@ -99,6 +102,7 @@ main = runTestTTAndExit $ TestList [ problem1
                                    , problem27
                                    , problem32
                                    , problem35
+                                   , problem36
                                    , problem50
                                    , problem87
                                    ]
