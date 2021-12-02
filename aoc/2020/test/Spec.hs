edccd44b363a0e7c426cd2b8d132baf8992ad9e3
Fabian Bull <fabian@fbull.de>
2021-12-02 17:15:19 +0100

    Add day 2 of the 2020 Advent of Code

--- aoc/2020/test/Spec.hs
+++ aoc/2020/test/Spec.hs
@@ -1,10 +1,21 @@
-import Test.HUnit
 import AdventOfCode.Day1 as Day1
+import AdventOfCode.Day2 as Day2
+import Test.HUnit
 
 problem1a = TestCase (Day1.solveA >>= assertEqual "Solution to problem 1a" 888331)
+
 problem1b = TestCase (Day1.solveB >>= assertEqual "Solution to problem 1b" 130933530)
 
+problem2a = TestCase (Day2.solveA >>= assertEqual "Solution to problem 2a" 607)
+
+problem2b = TestCase (Day2.solveB >>= assertEqual "Solution to problem 2b" 321)
+
 main :: IO ()
-main = runTestTTAndExit $ TestList [ problem1a
-                                   , problem1b
-                                   ]
+main =
+  runTestTTAndExit $
+    TestList
+      [ problem1a,
+        problem1b,
+        problem2a,
+        problem2b
+      ]
