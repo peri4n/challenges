f787c0b0042daa962c8037204a7ddb6d8141e295
Fabian Bull <fabian@fbull.de>
2021-12-07 18:57:58 +0100

    Add day 4 of the 2021 Advent of Code

--- aoc/2021/test/Spec.hs
+++ aoc/2021/test/Spec.hs
@@ -1,5 +1,6 @@
 import AdventOfCode.Day1 as Day1
 import AdventOfCode.Day2 as Day2
+import AdventOfCode.Day4 as Day4
 import Test.HUnit
 
 problem1a = TestCase (Day1.solveA >>= assertEqual "Solution to problem 1a" 1711)
@@ -10,6 +11,10 @@ problem2a = TestCase (Day2.solveA >>= assertEqual "Solution to problem 2a" 16601
 
 problem2b = TestCase (Day2.solveB >>= assertEqual "Solution to problem 2b" 1604592846)
 
+problem4a = TestCase (Day4.solveA >>= assertEqual "Solution to problem 4a" 58374)
+
+problem4b = TestCase (Day4.solveB >>= assertEqual "Solution to problem 4b" 11377)
+
 main :: IO ()
 main =
   runTestTTAndExit $
@@ -17,5 +22,7 @@ main =
       [ problem1a,
         problem1b,
         problem2a,
-        problem2b
+        problem2b,
+        problem4a,
+        problem4b
       ]
